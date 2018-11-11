8e7ae46e97522e25b2d151d4c4a74ce439767933
Ovsjah <ovsjah@gmail.com>
2018-11-12 01:16:07 +0200

    Refactor Permissions Module

--- sample.rb
+++ sample.rb
@@ -3,9 +3,9 @@ Table Users {
   Name
 }
 
-Table Rolls {
+Table Roles {
   Id,
-  Roll,
+  Role,
   User_id
 }
 
@@ -13,24 +13,24 @@ Table Permissions {
   Id,
   Permission,
   User_id,
-  Roll_id
+  Role_id
 }
 
 module Permissions
-  def grand(user, permissions, roll_id = nil)
+  def grand(user, permissions, role_id = nil)
     user = user.is_a?(User) ? user : User.find_by(name: user)
-    permissions.each { |permission| create_permission(user, permission, roll_id) }
+    permissions.each { |permission| create_permission(user, permission, role_id) }
   end
   
   def grand_via_role(name, role_name, permissions)
     user = User.find_by(name: name)
-    roll = user.rolls.create(role: role_name, user_id: user.id) if user
-    grand(user, permissions, roll.id) if roll
+    role = user.roles.create(role: role_name, user_id: user.id) if user
+    grand(user, permissions, role.id) if role
   end
   
-  def create_permission(user, permission, roll_id)
+  def create_permission(user, permission, role_id)
     return unless user
-    user.permissions.create(permission: permission, user_id: user.id, roll_id: roll_id)
+    user.permissions.create(permission: permission, user_id: user.id, role_id: role_id)
   end  
   
   def has_permision?(name, permission)
