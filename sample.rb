602c14f768d8b9c961cf19700d753df17371cce9
Ovsjah <ovsjah@gmail.com>
2018-11-12 00:20:57 +0200

    Refactor Permissions Module

--- sample.rb
+++ sample.rb
@@ -1,27 +1,33 @@
 Table Users {
   Id,
-  Name,
-  Permissions,
-  Rolls
+  Name
 }
 
+Table Rolls {
+  Id,
+  Roll,
+  User_id,
+  Permission_id
+}
+
+Table Permissions {
+  Id,
+  Permission,
+  User_id,
+  Roll_id
+}
 
 module Permissions
   def grand(name, permission)
-    user = User.find_by(name: name)
-    user.permissions.push(permission)
-    user.save
+    User.find_by(name: name)&.permissions&.create(permission: permission, user_id: user.id)
   end
   
   def grand_via_role(name, role_name, permission)
-    user = User.find_by(name: name)
-    role = Role.new(name, permission)
-    user.rolls.push(role)
-    user.permissions.push(role.permission)
-    user.save
+    new_permission = grand(name, permission)
+    User.find_by(name: name)&.rolls&.create(role: role_name, user_id: user.id, permission_id: new_permission.id)
   end
   
   def has_permision?(name, permission)
-    User.find_by(name: name).permissions.include?(permission)
+    !!User.find_by(name: name)&.permissions&.find_by(permission: permission)
   end
 end
